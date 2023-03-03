810593ccf7171810bb04ac28ed117773afa0b209
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2023-03-03 13:07:58 +0000

    Finsih Instrument X-Ray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -23,6 +23,10 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
+# X-RAY ----------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 # CloudWatch Logs ----
 import watchtower
 import logging
@@ -58,6 +62,10 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# X-RAY ----------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
 app = Flask(__name__)
 
 # HoneyComb ---------
@@ -65,6 +73,8 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+# X-RAY ----------
+XRayMiddleware(app, xray_recorder)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
