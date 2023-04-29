3f862eaaa7168a0e3e942ac7879be7e15dacf874
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2023-04-29 19:28:24 +0000

    fix some problems, avatars folder, flask problem in rollbar, seed.sql old problem, bootstrap doesn't work now

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,6 +3,7 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Abdassalam Ahmad','abdassalam.ahmad98@gmail.com' , 'Abdassalam' ,'MOCK'),
   ('Londo Londo','Londo@londo.co' , 'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
