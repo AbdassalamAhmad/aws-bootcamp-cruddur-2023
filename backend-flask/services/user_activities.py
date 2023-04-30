2510350321a77e387c251a8c5fcd0381766cde83
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2023-04-30 15:08:52 +0000

    Implement userfeedpage backend, sql and frontend

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,5 +1,5 @@
-from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.core import xray_recorder
+from lib.db import db
 
 class UserActivities:
   def run(user_handle):
@@ -8,27 +8,20 @@ class UserActivities:
       'errors': None,
       'data': None
     }
-
-    now = datetime.now(timezone.utc).astimezone()
     
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
+
+      sql = db.template('users','show_profile_and_its_activities')
+      results = db.query_object_json(sql, {'handle': user_handle})
       model['data'] = results
     
-    # xray ---
-    with xray_recorder.capture('user_activity_time') as subsegment1:
-      subsegment1.put_annotation('time_now', now.isoformat())
+    # # xray ---
+    # with xray_recorder.capture('user_activity_time') as subsegment1:
+    #   subsegment1.put_annotation('time_now', now.isoformat())
 
-      with xray_recorder.capture('user_activity_length') as subsegment2:
-        subsegment2.put_annotation("app_results_length", len(results))
+    #   with xray_recorder.capture('user_activity_length') as subsegment2:
+    #     subsegment2.put_annotation("app_results_length", len(results))
 
     return model
