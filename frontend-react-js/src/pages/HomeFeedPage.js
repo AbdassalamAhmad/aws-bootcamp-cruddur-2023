0b0426b00792f14a18f719a9ba3971aa2c73ef36
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2023-05-02 11:26:51 +0000

    edit project strucuture import, add python buffer

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -2,15 +2,18 @@ import './HomeFeedPage.css';
 import React from "react";
 
 // Authenication
-import {checkAuth, getAccessToken} from '../lib/CheckAuth';
+import {checkAuth, getAccessToken} from 'lib/CheckAuth';
 
-import DesktopNavigation  from '../components/DesktopNavigation';
-import DesktopSidebar     from '../components/DesktopSidebar';
-import ActivityFeed from '../components/ActivityFeed';
-import ActivityForm from '../components/ActivityForm';
-import ReplyForm from '../components/ReplyForm';
+import DesktopNavigation  from 'components/DesktopNavigation';
+import DesktopSidebar     from 'components/DesktopSidebar';
+import ActivityFeed from 'components/ActivityFeed';
+import ActivityForm from 'components/ActivityForm';
+import ReplyForm from 'components/ReplyForm';
+
 
 import { trace, context, } from '@opentelemetry/api';
+
+
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
